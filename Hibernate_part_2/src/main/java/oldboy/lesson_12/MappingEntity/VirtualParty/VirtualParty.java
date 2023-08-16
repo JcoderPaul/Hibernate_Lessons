ddac88995131c5e56614627553aba97f917cbcc2
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-08-16 21:03:28 +0300

    Add VirtualParty.java in Hibernate_part_2

---
+++ Hibernate_part_2/src/main/java/oldboy/lesson_12/MappingEntity/VirtualParty/VirtualParty.java
@@ -0,0 +1,32 @@
+package oldboy.lesson_12.MappingEntity.VirtualParty;
+
+import lombok.*;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(exclude = "jovial")
+@EqualsAndHashCode(exclude = "jovial")
+@Builder
+@Entity
+@Table(name = "virtual_parties", schema = "public")
+public class VirtualParty {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "party_id")
+    private Long partyId;
+
+    @Column(name = "party_name")
+    private String partyName;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "party")
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    private List<JovialAndParty> jovial = new ArrayList<>();
+}
