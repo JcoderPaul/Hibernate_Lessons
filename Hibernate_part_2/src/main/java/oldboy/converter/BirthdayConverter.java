ec86381f96f78d3539f51f35fb8426e4c78186c6
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-08-16 20:51:15 +0300

    Add BirthdayConverter.java in Hibernate_part_2

---
+++ Hibernate_part_2/src/main/java/oldboy/converter/BirthdayConverter.java
@@ -0,0 +1,35 @@
+package oldboy.converter;
+
+import oldboy.entity.accessory.Birthday;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Date;
+import java.util.Optional;
+
+/*
+Наш конвертер для удобства же нас, должен запускаться автоматически.
+Есть два способа настройки этого. Первый это аннотация с параметром.
+Второй это метод прописанный в нашем рабочем классе HibernateDemo.java
+*/
+@Converter(autoApply = true)
+public class BirthdayConverter implements AttributeConverter<Birthday, Date> {
+    /* Переопределяем методы */
+    /* Полученный "новый" или "самописный" тип нужно преобразовать в SQL */
+    @Override
+    public Date convertToDatabaseColumn(Birthday attribute) {
+        return Optional.ofNullable(attribute).
+                map((Birthday birthday) -> birthday.birthDate()).
+                map(date -> Date.valueOf(date)).
+                orElse(null);
+    }
+
+    /* Тип данных из БД будет преобразован в наш Birthday */
+    @Override
+    public Birthday convertToEntityAttribute(Date dbData) {
+        return Optional.ofNullable(dbData).
+                map(date -> date.toLocalDate()).
+                map(birthDate -> new Birthday(birthDate)).
+                orElse(null);
+    }
+}
