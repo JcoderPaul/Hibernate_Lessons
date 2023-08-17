35685404681ee2c491982571e250fcb313786fc0
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-08-17 19:52:47 +0300

    Add Schoolboy.java in Hibernate_part_9

---
+++ Hibernate_part_9/src/main/java/oldboy/entity/Schoolboy.java
@@ -0,0 +1,31 @@
+package oldboy.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "schoolboys", schema = "part_four_base")
+public class Schoolboy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long Id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "scholarship", nullable = false)
+    private Integer scholarship;
+}
