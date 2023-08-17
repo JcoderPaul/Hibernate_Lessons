8939cd5c8f8b408e0306e9c25a66a8add2786214
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-08-17 09:10:57 +0300

    Add MachineBodyDetail.java in Hibernate_part_3

---
+++ Hibernate_part_3/src/main/java/oldboy/lesson_18/Entity_18/Enums/MachineBodyDetail.java
@@ -0,0 +1,16 @@
+package oldboy.lesson_18.Entity_18.Enums;
+
+public enum MachineBodyDetail {
+    FENDER_FRONT_RIGHT,
+    FENDER_FRONT_LEFT,
+    REAR_RIGHT_FENDER,
+    REAR_FENDER_LEFT,
+    FRONT_RIGHT_DOOR,
+    REAR_RIGHT_DOOR,
+    FRONT_LEFT_DOOR,
+    REAR_LEFT_DOOR,
+    TRUNK,
+    HOOD,
+    FRONT_BUMPER,
+    REAR_BUMPER
+}
