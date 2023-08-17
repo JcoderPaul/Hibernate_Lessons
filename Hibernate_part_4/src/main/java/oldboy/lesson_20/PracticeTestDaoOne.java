5f610cd7ee92b612fbb839ea7650d59de5a17f92
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-08-17 16:54:47 +0300

    Add PracticeTestDaoOne.java in Hibernate_part_4

---
+++ Hibernate_part_4/src/main/java/oldboy/lesson_20/PracticeTestDaoOne.java
@@ -0,0 +1,34 @@
+package oldboy.lesson_20;
+
+import oldboy.Util.HibernateUtil;
+import oldboy.dao.UserDao;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class PracticeTestDaoOne {
+    public static void main(String[] args) {
+        try(SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
+            Session currentSession = sessionFactory.openSession()) {
+            currentSession.beginTransaction();
+            UserDao udTest = UserDao.getInstance();
+
+            System.out.println("------------------------ Test 1 ------------------------");
+            var queryRes = udTest.findAll(currentSession);
+            queryRes.forEach(System.out::println);
+
+            System.out.println("------------------------ Test 2 ------------------------");
+            var queryRes_2 = udTest.findAllByFirstName(currentSession, "Willy");
+            queryRes_2.forEach(System.out::println);
+
+            System.out.println("------------------------ Test 3 ------------------------");
+            var queryRes_3 = udTest.findLimitedUsersOrderedByBirthday(currentSession, 5);
+            queryRes_3.forEach(System.out::println);
+
+            System.out.println("------------------------ Test 4 ------------------------");
+            var queryRes_4 = udTest.findAllByCompanyName(currentSession, "Google");
+            queryRes_4.forEach(System.out::println);
+
+            currentSession.getTransaction().commit();
+        }
+    }
+}
