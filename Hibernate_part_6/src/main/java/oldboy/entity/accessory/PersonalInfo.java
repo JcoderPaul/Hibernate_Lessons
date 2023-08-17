d930b787182de409c2e3504a60b1dbd01ecd516e
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-08-17 17:37:58 +0300

    Add PersonalInfo.java in Hibernate_part_6

---
+++ Hibernate_part_6/src/main/java/oldboy/entity/accessory/PersonalInfo.java
@@ -0,0 +1,22 @@
+package oldboy.entity.accessory;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Embeddable
+public class PersonalInfo implements Serializable {
+
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+}
