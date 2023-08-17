e214a16168e017487f83e4bfccb5e4c742f8ee55
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-08-17 09:10:37 +0300

    Add EngineModel.java in Hibernate_part_3

---
+++ Hibernate_part_3/src/main/java/oldboy/lesson_18/Entity_18/Enums/EngineModel.java
@@ -0,0 +1,13 @@
+package oldboy.lesson_18.Entity_18.Enums;
+
+public enum EngineModel {
+    BMW_R3_B38,
+    BMW_R4_M10,
+    BMW_R4_M40,
+    BMW_V12_M70,
+    BMW_V12_M73,
+    AUDI_EA111_1_2_TFSI_CBZA,
+    AUDI_EA111_1_2_TFSI_CBZB,
+    AUDI_EA398_6_0L_BHT,
+    AUDI_EA398_6_3L_CEJA
+}
