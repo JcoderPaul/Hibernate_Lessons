ca27fb9407a6a4ad2ff370bd0ebbaaa3be44d76c
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-08-17 09:01:00 +0300

    Add Chat.java in Hibernate_part_3

---
+++ Hibernate_part_3/src/main/java/oldboy/entity/Chat.java
@@ -0,0 +1,28 @@
+package oldboy.entity;
+
+import lombok.*;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "chatName")
+@ToString(exclude = "userChats")
+@Builder
+@Entity
+@Table(name = "chats", schema = "training_base")
+public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chat_id")
+    private Long chatId;
+
+    @Column(name = "chat_name", nullable = false, unique = true)
+    private String chatName;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "chat")
+    private Set<UserChat> userChats = new HashSet<>();
+}
