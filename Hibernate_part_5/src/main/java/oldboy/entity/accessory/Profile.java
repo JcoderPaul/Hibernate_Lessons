1685a26be879e4ccbe13fc0b09f8cf4dac914612
Paul <100008373+JcoderPaul@users.noreply.github.com>
2023-08-17 17:16:45 +0300

    Add Profile.java in Hibernate_part_5

---
+++ Hibernate_part_5/src/main/java/oldboy/entity/accessory/Profile.java
@@ -0,0 +1,32 @@
+package oldboy.entity.accessory;
+
+import lombok.*;
+import oldboy.entity.User;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "profile", schema = "part_four_base")
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "profile_id")
+    private Long profileId;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String street;
+
+    private String language;
+
+    public void setUser(User user) {
+        user.setProfile(this);
+        this.user = user;
+    }
+}
